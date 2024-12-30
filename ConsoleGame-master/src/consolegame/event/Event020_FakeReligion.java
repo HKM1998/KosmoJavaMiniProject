dc59976f983ebf467f8f28c289f6a2addb4f2c94
HKM1998 <86611859+HKM1998@users.noreply.github.com>
2024-12-30 10:00:27 +0900

    241230_1000
    이벤트 수정내용 커밋

--- ConsoleGame-master/src/consolegame/event/Event020_FakeReligion.java
+++ ConsoleGame-master/src/consolegame/event/Event020_FakeReligion.java
@@ -3,6 +3,7 @@ package consolegame.event;
 import consolegame.character.Character;
 import consolegame.console.ConsolePrint;
 import consolegame.item.Item;
+import consolegame.item.Item002_Bible;
 
 public class Event020_FakeReligion extends Event {
 	public Event020_FakeReligion() {
@@ -19,9 +20,9 @@ public class Event020_FakeReligion extends Event {
 		Selection selection = new Selection();
 
 		selection.addSelection("1.그냥 들어본다.");
-		
+
 		selection.addSelection("2.그냥 간다.");
-		
+
 		// 특정 아이템 조회 후 사용
 //		if (Item.hasItem(c, 000))
 //			// 아이템 id 가 000인 아이템이 있는지
@@ -35,7 +36,6 @@ public class Event020_FakeReligion extends Event {
 //				
 //			}
 //		}
-
 
 		selection.print();
 	}
@@ -56,24 +56,17 @@ public class Event020_FakeReligion extends Event {
 
 	@Override
 	public void getResult(Character c, String pChoice) {
-		// 0번 아이템ID 가 있는 경우 실행
-//		if (Item.hasItem(c, 000)) {
-//			c.removeItem(000);
-//		}
-//
-//		// 무기가 있는경우 실행
-//		if (Item.hasItemType(c, "무기")) {
-//		}
-		
-		
-		
-	if (pChoice.equals("1")) {	                  //1번 선택 시 나오는 대사 추가
-			StringBuilder script = new StringBuilder();                   //1번 선택시 Good 엔딩 루트의 Bad엔딩 선결조건 중 하나
-			script.append(getEventId() + ". " + getName() + "\n");
+		StringBuilder script = new StringBuilder(); // 1번 선택시 Good 엔딩 루트의 Bad엔딩 선결조건 중 하나
+		script.append(getEventId() + ". " + getName() + "\n");
+
+		if (pChoice.equals("1")) { // 1번 선택 시 나오는 대사 추가
 			script.append("일장연설을 듣고 나니 기가 다 빠진다.\n");
 			script.append("그러나 아줌마는 이것도 인연이라며 성경을 선물한다\n");
+			c.addItem(new Item002_Bible());
+			script.append("\n아줌마에게서 성경을 얻었습니다.(+ 성경)\n");
+		} else if (pChoice.equals("2")) {
+			script.append("당신은 너무 지친 나머지 이야기를 듣지않고 지나쳤습니다.\n");
 		}
-		
-		
+
 	}
 }
