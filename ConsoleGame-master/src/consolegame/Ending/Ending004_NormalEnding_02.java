386829db452c41c1c2a8f7e087ff7dd8b7962eac
HKM1998 <86611859+HKM1998@users.noreply.github.com>
2024-12-31 17:59:31 +0900

    Revert "Merge branch 'main' of https://github.com/HKM1998/KosmoJavaMiniProject"
    This reverts commit 591cec4b0b5e7b05fb851139a56f0d52f9e487cf, reversing
    changes made to 399e3e944d4064c716acb67013a3cb1958bcbe44.

--- ConsoleGame-master/src/consolegame/Ending/Ending004_NormalEnding_02.java
+++ ConsoleGame-master/src/consolegame/Ending/Ending004_NormalEnding_02.java
@@ -1,38 +1,10 @@
 package consolegame.Ending;
 
-import consolegame.console.ConsolePrint;
-
 public class Ending004_NormalEnding_02 extends Ending{
 
 	@Override
 	public void printScript() {
-		StringBuilder script = new StringBuilder();
-		script.append("\n당신은 길을 걷다 우연히 쉘터를 발견하게 되었습니다.\n");
-		script.append("쉘터의 내부창고엔 10년은 거뜬히 먹을수있을 양의 보존식량과.\n");
-		script.append("각종 통조림, 그리고 식량이 산처럼 쌓여있습니다.\n");
-		script.append("\n그리고 쉘터의 다른 창고엔 생존에 필요한 도구와 무기가 가득합니다.\n");
-		script.append("당신은 이렇게 완벽한 장소에 사람이 아무도 없단걸 인지하려던 찰나에\n");
-		script.append("꺼림칙한 느낌이 들어 뒤를 돌아본 순간.\n");
-		script.append("당신은 탁 쏘는 강렬한 약품 냄새가 입과 코로 밀려들어오는 느낌을 받고\n");
-		script.append("정신이 혼미해집니다.\n");
-		script.append("\n당신의 앞엔 피부가 녹아내린듯한\n");
-		script.append("남성인지 여성인지도 알수가없는 끔찍한 몰골의 괴한이 서있습니다.\n");
-		script.append("그 괴한의 말을 끝으로 당신은 정신을 잃게 됩니다.\n");
-		script.append("\n이 히히히 남자다 히히히ㅣㅎ .\n");
-		script.append("\n에햏헤ㅎ ㅔㅎ 남자 냄새 히히ㅣㅎ .\n");
-		script.append("\n이히히히 내꺼다 히히히ㅣㅎ .\n");
-		script.append(" \n");
-		script.append("\n\n/////  r\n"
-				+ "    |  o   o  |\r\n"
-				+ "    |    -^-     |\r\n"
-				+ "    |  \\__()_   |\r\n"
-				+ "     \\___%____/\r\n"
-				+ "       |     |\r\n"
-				+ "      /       \\\r\n"
-				+ "     /_________\\");
-		
-		ConsolePrint.printScript(script);
-	
+		// TODO Auto-generated method stub
 	}
-	
+
 }
