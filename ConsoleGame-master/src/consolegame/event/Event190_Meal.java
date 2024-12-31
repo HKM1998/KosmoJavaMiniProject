ad378839dc1333f8d4bb24db086b6e4b15e81a98
jjokonae <kimhn2728@kakao.com>
2024-12-31 11:44:21 +0900

    2412311144
    이벤트 15,19 수정중

--- ConsoleGame-master/src/consolegame/event/Event190_Meal.java
+++ ConsoleGame-master/src/consolegame/event/Event190_Meal.java
@@ -28,7 +28,7 @@ public class Event190_Meal extends Event {
 		script.append(getEventId() + ". " + getName() + "\n");
 		script.append("당신은 우연히 거리를 걷다 주변 건물의 잔해에서 한 여자아이와 마주쳤다.\n");
 		script.append("여자아이는 당신을 발견하곤 먹을걸 달라는듯 쫒아온다.\n");
-		script.append("어떻할까?");
+		script.append("어떻할까?\n");
 		ConsolePrint.printScript(script, getIsLoaded());
 	}
 
@@ -37,17 +37,17 @@ public class Event190_Meal extends Event {
 		StringBuilder script = new StringBuilder();
 		if (pChoice.equals("1")) {
 			script.append("줄게 이런것 밖에 없구나. \n");
-			script.append("미안하구나.잘 가렴");
+			script.append("미안하구나.잘 가렴\n");
 			Main.character.removeItem(10);
 
 		} else if (pChoice.equals("2")) {
 			script.append("뭐야 이건? 꺼져\n");
-			script.append("당신은 접근하는 소녀를 발로 걷어찼다.(+쓰레기짓)");
+			script.append("당신은 접근하는 소녀를 발로 걷어찼다.(+쓰레기짓)\n");
 			Main.character.addItem(new Item012_Thash()); // 쓰레기
 		} else if (pChoice.equals("3")) {
 			script.append("여자아이는 별다른 저항도 하지못한채로\n");
 			script.append("당신은 여자아이의 모든걸 빼았았다.\n ");
-			script.append("당신은 상쾌함과 뿌듯함을 느꼈다. 정말 쓰레기인듯 하다.(체력 -1, 정신력 -1, +쓰레기짓) ");
+			script.append("당신은 상쾌함과 뿌듯함을 느꼈다. 정말 쓰레기인듯 하다.(체력 -1, 정신력 -1, +쓰레기짓) \n");
 			Main.character.setHealth(Main.character.getHealth() - 1);
 			Main.character.setMental(Main.character.getMental() - 1);
 			Main.character.addItem(new Item012_Thash()); // 쓰레기
