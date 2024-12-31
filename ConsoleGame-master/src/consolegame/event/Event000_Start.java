46eaecf56d441d93929fe9522ec79f14d9dcaeb6
jjokonae <kimhn2728@kakao.com>
2024-12-31 10:10:15 +0900

    2412311010
    이벤트16 텍스트 수정

--- ConsoleGame-master/src/consolegame/event/Event000_Start.java
+++ ConsoleGame-master/src/consolegame/event/Event000_Start.java
@@ -27,8 +27,8 @@ public class Event000_Start extends Event {
 		script.append("평범한 여느 남자 아이들처럼 사고를 치기도 하며 사랑을 받기도 하며 자라났습니다. \n");
 		script.append("그렇게 자라나 막 성인이 된 당신은 마을 여기 저기서 일을 하게 됩니다.\n");
 		script.append("그러나 어느 순간 열지 말아야 할 판도라의 상자를 열게 되는데 \n");
-		script.append("촌장의 뒷뜰에 몰래 재배 되고 있는 아편을 발견합니다.\n");
-		script.append("그리곤 이내 이 아편을 어떻게 할지 고민에 빠지게 됩니다.\n");
+		script.append("촌장의 뒷뜰에 몰래 재배 되고 있는 아편 밭을 발견합니다.\n");
+		script.append("그리곤 이내 이 아편들을 어떻게 할지 고민에 빠지게 됩니다.\n");
 
 		ConsolePrint.printScript(script, getIsLoaded());
 	}
