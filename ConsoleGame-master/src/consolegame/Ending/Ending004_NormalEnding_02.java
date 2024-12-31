b9be75102bb4ee0ad65d8e8e967e2fb683b44d13
HKM1998 <86611859+HKM1998@users.noreply.github.com>
2024-12-31 17:54:15 +0900

    Revert "2412311745"
    This reverts commit d98d6930cf614df10e04f43eef61bee6a2673201.

--- ConsoleGame-master/src/consolegame/Ending/Ending004_NormalEnding_02.java
+++ ConsoleGame-master/src/consolegame/Ending/Ending004_NormalEnding_02.java
@@ -7,15 +7,13 @@ public class Ending004_NormalEnding_02 extends Ending{
 	@Override
 	public void printScript() {
 		StringBuilder script = new StringBuilder();
-		script.append("\n당신은 계속 떠돌며 방황하다 우연히 쉘터를 발견하게 되었습니다.\n");
-		script.append("고된 여정으로 인해 지친 몸을 이끌고 겁도 없이 쉘터 내부로 들어갔습니다.\n");
-		script.append("\n쉘터의 내부창고엔 10년은 거뜬히 먹을수있을 양의 보존식량과.\n");
+		script.append("\n당신은 길을 걷다 우연히 쉘터를 발견하게 되었습니다.\n");
+		script.append("쉘터의 내부창고엔 10년은 거뜬히 먹을수있을 양의 보존식량과.\n");
 		script.append("각종 통조림, 그리고 식량이 산처럼 쌓여있습니다.\n");
-		script.append("\n당신은 너무 신이 난 나머지 경계심을 풀고 쉘터의 이곳 저곳을 돌아다닙니다.\n");
-		script.append("\n쉘터의 다른 창고를 둘러보니 이곳엔 생존에 필요한 도구와 무기가 가득합니다.\n");
-		script.append("당신은 이렇게 완벽한 장소에 사람이 아무도 없단걸 알고 자신의 거처로 삼으려던 찰나에\n");
+		script.append("\n그리고 쉘터의 다른 창고엔 생존에 필요한 도구와 무기가 가득합니다.\n");
+		script.append("당신은 이렇게 완벽한 장소에 사람이 아무도 없단걸 인지하려던 찰나에\n");
 		script.append("꺼림칙한 느낌이 들어 뒤를 돌아본 순간.\n");
-		script.append("\n당신은 탁 쏘는 강렬한 약품 냄새가 입과 코로 밀려들어오는 느낌을 받고\n");
+		script.append("당신은 탁 쏘는 강렬한 약품 냄새가 입과 코로 밀려들어오는 느낌을 받고\n");
 		script.append("정신이 혼미해집니다.\n");
 		script.append("\n당신의 앞엔 피부가 녹아내린듯한\n");
 		script.append("남성인지 여성인지도 알수가없는 끔찍한 몰골의 괴한이 서있습니다.\n");
@@ -24,16 +22,15 @@ public class Ending004_NormalEnding_02 extends Ending{
 		script.append("\n에햏헤ㅎ ㅔㅎ 남자 냄새 히히ㅣㅎ .\n");
 		script.append("\n이히히히 내꺼다 히히히ㅣㅎ .\n");
 		script.append(" \n");
-		script.append("\n///// // ///////r\n"
-				+ "    |  o   o /// |\r\n"
+		script.append("\n\n/////  r\n"
+				+ "    |  o   o  |\r\n"
 				+ "    |    -^-     |\r\n"
 				+ "    |  \\__()_   |\r\n"
 				+ "     \\___%____/\r\n"
 				+ "       |     |\r\n"
 				+ "      /       \\\r\n"
 				+ "     /_________\\");
-		script.append("\n 키히 ㅎ히히히 \n");
-		script.append("\n 괴한의 소름 끼치는 목소리와 함께 당신의 기억은 여기서 끊어지고 맙니다. \n");
+		
 		ConsolePrint.printScript(script);
 	
 	}
--- ConsoleGame-master/src/consolegame/event/Event000_Start.java
+++ ConsoleGame-master/src/consolegame/event/Event000_Start.java
@@ -28,13 +28,13 @@ public class Event000_Start extends Event {
 		StringBuilder script = new StringBuilder();
 		script.append(getEventId() + ". " + getName() + "\n");
 		script.append("\"3차세계 대전 이후 폐허가 된 이후의 세계인 2055년,\n");
-		script.append("당신은 특별한 재주나 능력은 없어도 그저 성실히 버려진 물건들을 줍고 팔아가며 생활하고 있습니다.\n");
-		script.append("늘 그렇듯 자주가던 뒷산에서 무언가 쓸만한게 없나 찾아보던 찰나에 그날따라 유난히 안가던 길로 가고 싶어합니다.\n");
-		script.append("그래서 길이 나지 않은 곳으로 깊이 들어갑니다.\n");
+		script.append("당신은 특별한 재주나 능력은 없어도 그저 성실히 살아가고 있습니다.\n");
+		script.append("늘 그렇듯 뒷산에서 산책을 하다가 그날따라 유난히 옆길로 새고 싶어합니다.\n");
+		script.append("그래서 산길이 나지 않은 곳으로 깊이 들어갑니다.\n");
 		script.append("그런데 당신은 뒷산에서 동네 갱단이 몰래 재배하고 있는 아편밭을 발견합니다.\n");
 		script.append("아편 밭의 옆엔 돈가방과 포장된 약 한무더기가 보입니다.\n");
 		script.append("세상에 마약이라니!\n");
-		script.append("당신은 이 마약과 돈들을 어떻게 할지 고민에 빠지게 됩니다.\n\n");
+		script.append("당신은 이 마약들을 어떻게 할지 고민에 빠지게 됩니다.\n\n");
 		script.append("불태우고 돈만들고 튈 것인가? 마약과 돈 전부 들고 튈 것인가?\n");
 
 		ConsolePrint.printScript(script, getIsLoaded());
@@ -45,13 +45,13 @@ public class Event000_Start extends Event {
 		StringBuilder script = new StringBuilder();
 
 		if (pChoice.equals("1")) {
-			script.append("당신은 마약에 불을 질렀습니다! 그리고 돈가방을 챙기고 여행을 떠납니다.\n");
+			script.append("당신은 마약에 불을 질렀습니다! 그리고 돈가방을 챙기고 도망을 칩니다.\n");
 			script.append("(소지금 +500)\n");
 			Main.character.setMoney(Main.character.getMoney()+500);
 		}
 
 		if (pChoice.equals("2")) {
-			script.append("당신은 어차피 없앨 마약이라면 본인이 가져가는게 낫다라 생각하고 돈가방도 챙겨 떠납니다.\n");
+			script.append("당신은 어차피 없앨 마약이라면 본인이 가져가는게 낫다라 생각하고 돈가방도 챙겨 도망칩니다.\n");
 			script.append("(소지금 +500)\n");
 			script.append("(+ 약)\n");
 			Main.character.setMoney(Main.character.getMoney() + 500);
